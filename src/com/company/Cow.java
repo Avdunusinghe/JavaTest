94bb0053ddfb7b87f4d4bbe4355ddd73f952fcbc
avdunusinghe <avdunusinghe@gmail.com>
2021-02-24 20:18:20 +0530

    reset

--- src/com/company/Cow.java
+++ src/com/company/Cow.java
@@ -1,5 +1,12 @@
 package com.company;
 
-public class Cow {
+public class Cow extends Animal {
 
+    public Cow(){}
+    public Cow(String name){
+        super(name);
+    }
+    public void speak() {
+        System.out.println("Moo... Moo...");
+    }
 }
